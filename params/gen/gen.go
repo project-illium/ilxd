deab3e47aab81dc8f17f1e4831364b843efb59c4
Chris Pacia <ctpacia@gmail.com>
2023-09-18 13:19:29 -0400

    Refactor params gen function

--- params/gen/gen.go
+++ params/gen/gen.go
@@ -5,14 +5,14 @@
 package main
 
 import (
-	"encoding/hex"
+	"crypto/rand"
 	"encoding/json"
 	"fmt"
 	"github.com/jessevdk/go-flags"
 	"github.com/libp2p/go-libp2p/core/crypto"
 	"github.com/libp2p/go-libp2p/core/peer"
 	"github.com/project-illium/ilxd/blockchain"
-	ilxcrypto "github.com/project-illium/ilxd/crypto"
+	icrypto "github.com/project-illium/ilxd/crypto"
 	params2 "github.com/project-illium/ilxd/params"
 	"github.com/project-illium/ilxd/repo/mock"
 	"github.com/project-illium/ilxd/types"
@@ -20,38 +20,18 @@ import (
 	"github.com/project-illium/ilxd/types/transactions"
 	"github.com/project-illium/walletlib"
 	"log"
+	"strings"
 )
 
 type GenerationParams struct {
-	ValidatorKey            string `long:"validatorkey" description:"The genesis validator private key"`
-	ViewKey                 string `long:"viewkey" description:"The genesis view public key"`
-	InitialCoins            uint64 `long:"initialcoins" description:"The number of coins created by the genesis block"`
-	CoinbaseUnlockingScript struct {
-		ScriptCommitment string   `long:"unlockingscript.scriptcommitment" description:"The coinbase's 0th output's unlocking script commitment"`
-		ScriptParams     []string `long:"unlockingscript.params" description:"The coinbase's 0th output's unlocking script params"`
-	}
-	CoinbaseNote struct {
-		ScriptHash string `long:"coinbasenote.scripthash" description:"The coinbase's 0th output's' note's' script hash"`
-		Amount     uint64 `long:"coinbasenote.amount" description:"The coinbase's 0th output's' note's amount"`
-		AssetID    string `long:"coinbasenote.assetid" description:"The coinbase's 0th output's' note's amount"`
-		State      string `long:"coinbasenote.state" description:"The coinbase's 0th output's' note's state'"`
-		Salt       string `long:"coinbasenote.salt" description:"The coinbase's 0th output's' note's salt'"`
-	}
-	AdditionalCoinbaseOutputs []transactions.Output
-	Timestamp                 int64 `long:"timestamp" description:"The genesis block timestamp"`
+	Mnemonic     string `long:"mnemonicseed" description:"The mnemonic seed to use when creating the params"`
+	InitialCoins uint64 `long:"initialcoins" description:"The number of coins created by the genesis block"`
+	Timestamp    int64  `long:"timestamp" description:"The genesis block timestamp"`
+	NetParams    string `long:"params" description:"The network params to use: [mainnet, testnet1, regtest, alphanet]"`
 }
 
 // --timestamp=1686874710
-// --validatorkey=080112401e08383f629522149a7505c7668f070bea3007af8ed2b3970e7a2f2456584039de64446ff3730ddbf2219fe1b996316650b747ea90d742c8e22f97bd3c0b616f
-// --viewkey=08041220fd1264c92cc8d0b4fc8bdb811bbbc487734990c85cb42e333d6c86d95ce8b16b
 // --initialcoins=230584300921369395
-// --unlockingscript.scriptcommitment=0000000000000000000000000000000000000000000000000000000000000000
-// --unlockingscript.params=0000000000000000000000000000000000000000000000000000000000000000
-// --coinbasenote.scripthash=ae09db7cd54f42b490ef09b6bc541af688e4959bb8c53f359a6f56e38ab454a3
-// --coinbasenote.amount=115292150460684697
-// --coinbasenote.assetid=0000000000000000000000000000000000000000000000000000000000000000
-// --coinbasenote.state=0000000000000000000000000000000000000000000000000000000000000000
-// --coinbasenote.salt=09969db4b1ee03587e33c0e66a99fabbde33e3d10dd9642f9493cfac399956fd
 
 func main() {
 	var params GenerationParams
@@ -61,85 +41,26 @@ func main() {
 		log.Fatal(err)
 	}
 
-	kc, _ := walletlib.NewKeychain(mock.NewMapDatastore(), &params2.RegestParams, params2.RegtestMnemonicSeed)
-
-	networkKey, _ := kc.NetworkKey()
-	sk, _ := crypto.MarshalPrivateKey(networkKey)
-	params.ValidatorKey = hex.EncodeToString(sk)
-
-	keys, _ := kc.PrivateKeys()
-	var ul types.UnlockingScript
-	var sh types.ID
-	var vieKey crypto.PubKey
-	for k := range keys {
-		spendKey := k.SpendKey()
-		vieKey = k.ViewKey().GetPublic()
-		sk2, _ := crypto.MarshalPublicKey(vieKey)
-
-		params.CoinbaseUnlockingScript.ScriptCommitment = hex.EncodeToString(walletlib.MockBasicUnlockScriptCommitment)
-		params.ViewKey = hex.EncodeToString(sk2)
-		params.ValidatorKey = hex.EncodeToString(sk)
-
-		raw, _ := spendKey.GetPublic().Raw()
-		params.CoinbaseUnlockingScript.ScriptParams = []string{hex.EncodeToString(raw)}
-
-		ul = types.UnlockingScript{
-			ScriptCommitment: walletlib.MockBasicUnlockScriptCommitment,
-			ScriptParams:     [][]byte{raw},
-		}
-		sh = ul.Hash()
-		params.CoinbaseNote.ScriptHash = hex.EncodeToString(sh[:])
+	var netParams *params2.NetworkParams
+	switch strings.ToLower(params.NetParams) {
+	case "mainnet":
+		netParams = &params2.MainnetParams
+	case "testnet1":
+		netParams = &params2.Testnet1Params
+	case "regtest":
+		netParams = &params2.RegestParams
+	case "alphanet":
+		netParams = &params2.AlphanetParams
+	default:
+		log.Fatal("unknown net params")
 	}
 
-	note2 := types.SpendNote{
-		ScriptHash: sh[:],
-		Amount:     115292150460684697,
-		AssetID:    types.IlliumCoinID,
-		State:      [128]byte{},
-	}
-	salt2, _ := hex.DecodeString("d9348297fbcc1a2aa1591701b6a749d253789b5ef97ef576b8b931a3093e07f9")
-	copy(note2.Salt[:], salt2)
-
-	commitment2 := note2.Commitment()
-
-	ct, _ := vieKey.(*ilxcrypto.Curve25519PublicKey).Encrypt(note2.Serialize())
-
-	params.AdditionalCoinbaseOutputs = []transactions.Output{
-		{
-			Commitment: commitment2[:],
-			Ciphertext: ct,
-		},
-	}
-
-	blk := &blocks.Block{
-		Header: &blocks.BlockHeader{
-			Version:     1,
-			Height:      0,
-			Parent:      make([]byte, 32),
-			Timestamp:   params.Timestamp,
-			TxRoot:      nil,
-			Producer_ID: nil,
-			Signature:   nil,
-		},
-		Transactions: make([]*transactions.Transaction, 2),
-	}
-
-	validatorKeyBytes, err := hex.DecodeString(params.ValidatorKey)
+	kc, err := walletlib.NewKeychain(mock.NewMapDatastore(), netParams, params.Mnemonic)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	validatorKey, err := crypto.UnmarshalPrivateKey(validatorKeyBytes)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	viewKeyBytes, err := hex.DecodeString(params.ViewKey)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	viewKey, err := crypto.UnmarshalPublicKey(viewKeyBytes)
+	validatorKey, err := kc.NetworkKey()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -153,116 +74,122 @@ func main() {
 		log.Fatal(err)
 	}
 
-	scriptCommitmentBytes, err := hex.DecodeString(params.CoinbaseUnlockingScript.ScriptCommitment)
+	keys, err := kc.PrivateKeys()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	coinbaseNoteScriptHash, err := hex.DecodeString(params.CoinbaseNote.ScriptHash)
-	if err != nil {
-		log.Fatal(err)
-	}
-	coinbaseNoteAssetID, err := hex.DecodeString(params.CoinbaseNote.AssetID)
-	if err != nil {
-		log.Fatal(err)
-	}
-	coinbaseNoteState, err := hex.DecodeString(params.CoinbaseNote.State)
-	if err != nil {
-		log.Fatal(err)
-	}
-	coinbaseNoteSalt, err := hex.DecodeString(params.CoinbaseNote.Salt)
-	if err != nil {
-		log.Fatal(err)
-	}
+	var (
+		spendKey     crypto.PrivKey
+		viewKey      crypto.PrivKey
+		stakeAmt     = params.InitialCoins / 2
+		secondOutAmt = params.InitialCoins - stakeAmt
+	)
 
-	note := types.SpendNote{
-		ScriptHash: coinbaseNoteScriptHash,
-		Amount:     types.Amount(params.CoinbaseNote.Amount),
-		AssetID:    types.NewID(coinbaseNoteAssetID),
+	for k := range keys {
+		spendKey = k.SpendKey()
+		viewKey = k.ViewKey()
+		break
 	}
-	copy(note.State[:], coinbaseNoteState)
-	copy(note.Salt[:], coinbaseNoteSalt)
-
-	unlockingParams := make([][]byte, 0, len(params.CoinbaseUnlockingScript.ScriptParams))
-	for _, p := range params.CoinbaseUnlockingScript.ScriptParams {
-		params, err := hex.DecodeString(p)
-		if err != nil {
-			log.Fatal(err)
-		}
-		unlockingParams = append(unlockingParams, params)
+	spendPubBytes, err := spendKey.GetPublic().Raw()
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	unlockingScript := types.UnlockingScript{
-		ScriptCommitment: scriptCommitmentBytes,
-		ScriptParams:     unlockingParams,
+		ScriptCommitment: walletlib.MockBasicUnlockScriptCommitment,
+		ScriptParams:     [][]byte{spendPubBytes},
 	}
+	scriptHash := unlockingScript.Hash()
 
-	blk.Transactions[0] = transactions.WrapTransaction(&transactions.CoinbaseTransaction{
-		Validator_ID: validatorIDBytes,
-		NewCoins:     params.InitialCoins,
-		Outputs:      make([]*transactions.Output, 1),
-		Signature:    nil,
-		Proof:        nil, // TODO
-	})
-
-	if types.NewID(note.ScriptHash).Compare(unlockingScript.Hash()) != 0 {
-		log.Fatal("coinbase unlocking script does not match spend note script hash")
+	note0 := types.SpendNote{
+		ScriptHash: scriptHash.Bytes(),
+		Amount:     types.Amount(stakeAmt),
+		AssetID:    types.IlliumCoinID,
 	}
+	rand.Read(note0.Salt[:])
 
-	commitment := note.Commitment()
+	note1 := types.SpendNote{
+		ScriptHash: scriptHash.Bytes(),
+		Amount:     types.Amount(secondOutAmt),
+		AssetID:    types.IlliumCoinID,
+	}
+	rand.Read(note1.Salt[:])
 
-	cipherText, err := ilxcrypto.Encrypt(viewKey, note.Serialize())
+	ciphertext0, err := viewKey.GetPublic().(*icrypto.Curve25519PublicKey).Encrypt(note0.Serialize())
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	output0 := &transactions.Output{
-		Commitment: commitment[:],
-		Ciphertext: cipherText,
+	ciphertext1, err := viewKey.GetPublic().(*icrypto.Curve25519PublicKey).Encrypt(note1.Serialize())
+	if err != nil {
+		log.Fatal(err)
 	}
-	blk.Transactions[0].GetCoinbaseTransaction().Outputs[0] = output0
 
-	for _, out := range params.AdditionalCoinbaseOutputs {
-		blk.Transactions[0].GetCoinbaseTransaction().Outputs = append(blk.Transactions[0].GetCoinbaseTransaction().Outputs, &out)
+	nullifier := types.CalculateNullifier(0, note0.Salt, unlockingScript.ScriptCommitment, unlockingScript.ScriptParams...)
+
+	blk := &blocks.Block{
+		Header: &blocks.BlockHeader{
+			Version:     1,
+			Height:      0,
+			Parent:      make([]byte, 32),
+			Timestamp:   params.Timestamp,
+			TxRoot:      nil,
+			Producer_ID: nil,
+			Signature:   nil,
+		},
+		Transactions: []*transactions.Transaction{
+			transactions.WrapTransaction(&transactions.CoinbaseTransaction{
+				Validator_ID: validatorIDBytes,
+				NewCoins:     params.InitialCoins,
+				Outputs: []*transactions.Output{
+					{
+						Commitment: note0.Commitment().Bytes(),
+						Ciphertext: ciphertext0,
+					},
+					{
+						Commitment: note1.Commitment().Bytes(),
+						Ciphertext: ciphertext1,
+					},
+				},
+				Signature: nil,
+				Proof:     nil, // TODO
+			}),
+			transactions.WrapTransaction(&transactions.StakeTransaction{
+				Validator_ID: validatorIDBytes,
+				Amount:       stakeAmt,
+				Nullifier:    nullifier.Bytes(),
+				TxoRoot:      nil,
+				Locktime:     0,
+				Signature:    nil,
+				Proof:        nil, // TODO
+			}),
+		},
 	}
 
 	sigHash, err := blk.Transactions[0].GetCoinbaseTransaction().SigHash()
 	if err != nil {
 		log.Fatal(err)
 	}
-	sig, err := validatorKey.Sign(sigHash)
+	blk.Transactions[0].GetCoinbaseTransaction().Signature, err = validatorKey.Sign(sigHash)
 	if err != nil {
 		log.Fatal(err)
 	}
-	blk.Transactions[0].GetCoinbaseTransaction().Signature = sig
 
 	acc := blockchain.NewAccumulator()
-	for _, output := range blk.Transactions[0].GetCoinbaseTransaction().Outputs {
-		acc.Insert(output.Commitment, false)
-	}
-	txoRoot := acc.Root().Bytes()
+	acc.Insert(blk.Transactions[0].GetCoinbaseTransaction().Outputs[0].Commitment, false)
+	acc.Insert(blk.Transactions[0].GetCoinbaseTransaction().Outputs[1].Commitment, false)
 
-	nullifier := types.CalculateNullifier(0, note.Salt, unlockingScript.ScriptCommitment, unlockingScript.ScriptParams...)
-
-	blk.Transactions[1] = transactions.WrapTransaction(&transactions.StakeTransaction{
-		Validator_ID: validatorIDBytes,
-		Amount:       params.CoinbaseNote.Amount,
-		Nullifier:    nullifier[:],
-		TxoRoot:      txoRoot,
-		Locktime:     0,
-		Signature:    nil,
-		Proof:        nil, // TODO
-	})
+	blk.Transactions[1].GetStakeTransaction().TxoRoot = acc.Root().Bytes()
 
 	sigHash, err = blk.Transactions[1].GetStakeTransaction().SigHash()
 	if err != nil {
 		log.Fatal(err)
 	}
-	sig, err = validatorKey.Sign(sigHash)
+	blk.Transactions[1].GetStakeTransaction().Signature, err = validatorKey.Sign(sigHash)
 	if err != nil {
 		log.Fatal(err)
 	}
-	blk.Transactions[1].GetStakeTransaction().Signature = sig
 
 	merkleRoot := blockchain.TransactionsMerkleRoot(blk.Transactions)
 	blk.Header.TxRoot = merkleRoot[:]
