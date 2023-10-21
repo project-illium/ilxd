5a191f7f8f9008f67b0a9b55812fde8cd183276d
Chris Pacia <ctpacia@gmail.com>
2023-10-21 14:22:46 -0400

    Add skiprusttests build flag

--- crypto/nova_test.go
+++ crypto/nova_test.go
@@ -1,3 +1,6 @@
+//go:build !skiprusttests
+// +build !skiprusttests
+
 // Copyright (c) 2022 The illium developers
 // Use of this source code is governed by an MIT
 // license that can be found in the LICENSE file.
