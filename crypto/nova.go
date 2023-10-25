f103949019b41ab38bf9ef638e74dc76c87382f3
Chris Pacia <ctpacia@gmail.com>
2023-10-25 09:50:18 -0400

    Add windows to release.yaml

--- crypto/nova.go
+++ crypto/nova.go
@@ -8,25 +8,18 @@
 package crypto
 
 /*
-#cgo CFLAGS: -Irust/target/release
-#cgo LDFLAGS: -Lrust/target/release -lillium_crypto -ldl -lpthread -lgcc_s -lc -lm
-
+#cgo linux CFLAGS: -Irust/target/release
+#cgo linux LDFLAGS: -Lrust/target/release -lillium_crypto -ldl -lpthread -lgcc_s -lc -lm
+#cgo windows CFLAGS: -Irust/target/release
+#cgo windows LDFLAGS: -Lrust/target/release -lillium_crypto
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
-
 void generate_secret_key(uint8_t* out);
-
 void secret_key_from_seed(const uint8_t* seed, uint8_t* out);
-
 void priv_to_pub(const uint8_t* bytes, uint8_t* out);
-
 void sign(const uint8_t* privkey, const uint8_t* message_digest, uint8_t* out);
-
 bool verify(const uint8_t* pubkey, const uint8_t* message_digest, const uint8_t* sig_r, const uint8_t* sig_s);
-
-// Helper function to free memory allocated by Rust
-void free_memory(void* ptr);
 */
 import "C"
 import (
