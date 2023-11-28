08f04ae86e100c2d95bfcd751628c3e3fe710b47
Chris Pacia <ctpacia@gmail.com>
2023-11-27 21:53:10 -0500

    Fix lurk commit test

--- zk/commitment_test.go
+++ zk/commitment_test.go
@@ -5,13 +5,32 @@
 package zk
 
 import (
-	"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestLurkCommit(t *testing.T) {
-	h, err := LurkCommit("(cons 1 (cons 2 nil))")
-	assert.NoError(t, err)
-	fmt.Println(h)
+	tests := []struct {
+		expression string
+		expected   string
+	}{
+		{
+			expression: "(cons 1 (cons 2 nil))",
+			expected:   "17fcbf74593838c62db86d114e1bab3bce2e09daab2eef410c0c6745b2eef696",
+		},
+		{
+			expression: "1",
+			expected:   "0a427bccbd5078eccd71ac1d575074dfd18a1402a698d1335d2af21767d0331c",
+		},
+		{
+			expression: "555",
+			expected:   "005ba8d537935281635f6eba89dda12ef51eb18d9e31a5d27add717b570d3bc6",
+		},
+	}
+
+	for _, test := range tests {
+		h, err := LurkCommit(test.expression)
+		assert.NoError(t, err)
+		assert.Equal(t, test.expected, h.String())
+	}
 }
