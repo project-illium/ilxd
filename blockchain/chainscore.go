8161bd8f9616b1eee768f7ea640e30eb1b056446
Chris Pacia <ctpacia@gmail.com>
2024-02-08 13:12:27 -0500

    Set verifier in chainscore

--- blockchain/chainscore.go
+++ blockchain/chainscore.go
@@ -43,6 +43,7 @@ func (b *Blockchain) CalcChainScore(blks []*blocks.Block, flags ...BehaviorFlags
 		txoRootSet:        b.txoRootSet.Clone(),   // Reads from disk db, writes to cache only.
 		sigCache:          NewSigCache(DefaultSigCacheSize),
 		proofCache:        NewProofCache(DefaultProofCacheSize),
+		verifier:          b.verifier,
 		notificationsLock: sync.RWMutex{},
 		stateLock:         sync.RWMutex{},
 	}
