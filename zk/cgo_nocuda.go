2b4f81cf4cfc8729543d2f20e56fc8f997107c94
Chris Pacia <ctpacia@gmail.com>
2024-04-13 16:57:06 +0200

    Update mac CGO targets

--- zk/cgo_nocuda.go
+++ zk/cgo_nocuda.go
@@ -12,7 +12,7 @@ package zk
 #cgo linux LDFLAGS: -Lrust/target/release -Lrust-target-release-zk -l:libillium_zk.a -ldl -lpthread -lgcc_s -lc -lm -lssl -lcrypto -lstdc++
 #cgo windows CFLAGS: -Irust/target/release
 #cgo windows LDFLAGS: -Lrust/target/release -l:libillium_zk.lib
-#cgo darwin CFLAGS: -Irust/target/release
-#cgo darwin LDFLAGS: -Lrust/target/release -l:libillium_zk.a -lc++ -lssl -lcrypto -framework SystemConfiguration
+#cgo darwin CFLAGS: -Irust/target/release -Irust/target/aarch64-apple-darwin
+#cgo darwin LDFLAGS: -Lrust/target/release -Irust/target/aarch64-apple-darwin -l:libillium_zk.a -lc++ -lssl -lcrypto -framework SystemConfiguration
 */
 import "C"
