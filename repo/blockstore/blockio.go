c9e3f6d59131983c3385acc302163c3fe455d72c
Chris Pacia <ctpacia@gmail.com>
2024-07-11 22:42:06 +0200

    Fix lint

--- repo/blockstore/blockio.go
+++ repo/blockstore/blockio.go
@@ -51,16 +51,6 @@ const (
 	// must be less than 2^32 (4 GiB).  This is also why it's a typed
 	// constant.
 	maxBlockFileSize uint32 = 512 * 1024 * 1024 // 512 MiB
-
-	// blockLocSize is the number of bytes the serialized block location
-	// data that is stored in the block index.
-	//
-	// The serialized block location format is:
-	//
-	//  [0:4]  Block file (4 bytes)
-	//  [4:8]  File offset (4 bytes)
-	//  [8:12] Block length (4 bytes)
-	blockLocSize = 12
 )
 
 var (
@@ -82,11 +72,6 @@ type DatabaseError struct {
 // Error satisfies the error interface and prints human-readable errors.
 func (e DatabaseError) Error() string {
 	return e.Description
-}
-
-// ruleError creates an RuleError given a set of arguments.
-func ruleError(desc string) DatabaseError {
-	return DatabaseError{Description: desc}
 }
 
 // makeDbErr creates a database.Error given a set of arguments.
